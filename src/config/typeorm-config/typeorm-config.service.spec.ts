7667d6a8c1d83a4d187427406874e33eec102cdf
chenc <double_cl@163.com>
2022-08-17 10:59:29 +0800

    chore: update test case

---
+++ src/config/typeorm-config/typeorm-config.service.spec.ts
@@ -0,0 +1,27 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { TypeOrmConfigService } from '~/config/typeorm-config/typeorm-config.service';
+import { TypeOrmTestingModule } from '~/test-utils';
+
+describe('TypeormConfig', () => {
+  let service: TypeOrmConfigService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule(
+      TypeOrmTestingModule({
+        controllers: [],
+        providers: [TypeOrmConfigService],
+        entities: [],
+      }),
+    ).compile();
+    service = module.get<TypeOrmConfigService>(TypeOrmConfigService);
+  });
+
+  it('should be defined', function () {
+    expect(service).toBeDefined();
+  });
+
+  it('should be return typeorm config correct', async () => {
+    const config = await service.createTypeOrmOptions('chenc');
+    expect(config.type).toBeDefined();
+  });
+});
